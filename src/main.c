87d1825b4902dd36cc6fbc6f38b70fa26e7bb4e1
Simen <Simennj@outlook.com>
2018-04-21 09:50:49 +0200

    add simple i controller

--- src/main.c
+++ src/main.c
@@ -7,24 +7,26 @@
 int main(int argc, char **argv) {
 
     double k_p = 1;
-    double k_i = 1;
-    double k_d = 1;
+    double k_i = .25;
+    double k_d = 0;
 
     double target_position = 0;
+    double accumulated_displacement = 0;
 
     double dt;
     struct timespec now_time, last_time;
     clock_gettime(CLOCK_MONOTONIC, &last_time);
-    for (int i = 0; i < 1000;) {
+    for (int i = 0; i < 6000;) {
         clock_gettime(CLOCK_MONOTONIC, &now_time);
         dt = now_time.tv_sec - last_time.tv_sec + (now_time.tv_nsec - last_time.tv_nsec) / 1E9;
         if (dt > .01) {
             i++;
             last_time = now_time;
             double displacement = target_position - getFakeBoatPosition();
-            double motor_force = k_p * displacement;
+            accumulated_displacement = accumulated_displacement + displacement * dt;
+            double motor_force = k_p * displacement + k_i * accumulated_displacement;
             updateFakeBoat(dt, motor_force);
-            if (i % 5 == 0) {
+            if (i % 10 == 0) {
                 printFakeBoatState();
             }
         }
