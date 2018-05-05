6b72f1d0e31297e6d638168f0bb1df5dc485b269
Simen <Simennj@outlook.com>
2018-05-05 23:10:30 +0200

    remove drag on fake boat

--- src/fake_boat.c
+++ src/fake_boat.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "fake_boat.h"
 
-double drag = 1;
-
 double position = 1;
 double velocity = 0;
 double acceleration = 0;
@@ -13,7 +11,7 @@ double getBoatPosition() {
 }
 
 void updateBoat(double dt, double motor_force) {
-    acceleration = motor_force - drag * velocity;
+    acceleration = motor_force;
     velocity = velocity + acceleration * dt;
     position = position + velocity * dt;
 }
