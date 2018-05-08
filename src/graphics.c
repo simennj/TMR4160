fa52d551ff68f804e73937080695545427e93438
Simen <simennj@outlook.com>
2018-05-08 15:49:17 +0200

    add seperate shaders for graph

--- src/graphics.c
+++ src/graphics.c
@@ -4,6 +4,7 @@
 #include "shader_util.h"
 
 GLuint uiShaderProgram;
+GLuint graphShaderProgram;
 
 void graphics_reload();
 
@@ -20,10 +21,41 @@ GLuint graphVertexBuffer;
 GLint currentGraphVertexNumber;
 #define graphLength 1000
 
+
+void bindPolygons(GLfloat *verts, GLint vertCount, GLint *indic,
+                  GLint indicCount) {// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertCount * 3, verts, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLint) * indicCount * 3, indic, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER,
+                 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
+
+    glBindVertexArray(
+            0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
+}
 void ui_init() {
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
+}
+
+void ui_load() {
+    GLint vertCount = getVectorCountFromFile("vertices.txt");
+    GLint indicCount = getVectorCountFromFile("indices.txt");
+    GLfloat verts[vertCount * 3];
+    GLint indic[indicCount * 3];
+    getFloatVectorFromFile("vertices.txt", vertCount, verts);
+    getIntVectorFromFile("indices.txt", indicCount, indic);
+    bindPolygons(verts, vertCount, indic, indicCount);
+    triangles = indicCount;
 }
 
 void boat_init() {
@@ -47,6 +79,7 @@ void graph_init() {
     glEnableVertexAttribArray(0);
 
     uiShaderProgram = glCreateProgram();
+    graphShaderProgram = glCreateProgram();
 }
 
 void graphics_init(void *(*loadProc)(const char)) {
@@ -73,32 +106,12 @@ void graphics_update() {
     glDrawElements(GL_TRIANGLES, triangles * 3, GL_UNSIGNED_INT, 0);
     glBindVertexArray(boatVertexArray);
     glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glUseProgram(graphShaderProgram);
     glBindVertexArray(graphVertexArray);
     glDrawArrays(GL_LINE_STRIP, 0, currentGraphVertexNumber);
     glDrawArrays(GL_LINE_STRIP, currentGraphVertexNumber, graphLength - currentGraphVertexNumber);
     glBindVertexArray(0);
-}
-
-void bindPolygons(GLfloat *verts, GLint vertCount, GLint *indic,
-                  GLint indicCount) {// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertCount * 3, verts, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLint) * indicCount * 3, indic, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER,
-                 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-    glBindVertexArray(
-            0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs), remember: do NOT unbind the EBO, keep it bound to this VAO
-
-
 }
 
 void graphics_setBoatPosition(GLfloat position) {
@@ -114,12 +127,6 @@ void graphics_setBoatPosition(GLfloat position) {
 
 void graphics_reload() {
     shader_programInit(uiShaderProgram, ".");
-    GLint vertCount = getVectorCountFromFile("vertices.txt");
-    GLint indicCount = getVectorCountFromFile("indices.txt");
-    GLfloat verts[vertCount * 3];
-    GLint indic[indicCount * 3];
-    getFloatVectorFromFile("vertices.txt", vertCount, verts);
-    getIntVectorFromFile("indices.txt", indicCount, indic);
-    bindPolygons(verts, vertCount, indic, indicCount);
-    triangles = indicCount;
+    shader_programInit(graphShaderProgram, "graph");
+    ui_load();
 }
