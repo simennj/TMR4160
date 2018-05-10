1bfd5efeb0aaad96883a4be305ff41e04e6c63d8
Simen <Simennj@outlook.com>
2018-05-10 23:14:19 +0200

    add comments to ui source

--- src/ui.c
+++ src/ui.c
@@ -3,46 +3,59 @@
 #include "file_utils.h"
 #include "shader_util.h"
 
+// Define amount of coordinates per vertex
+#define VERTEX_COORDINATE_COUNT 3
+
+// OpenGL identifiers
 GLuint uiShaderProgram;
 GLuint uiVertexBuffer;
 GLuint uiVertexArray;
 GLuint uiElementArray;
+
+// Current amount of triangles to draw
 int uiTrianglesCount;
 
-void bindPolygons(GLfloat *verts, GLint vertCount, GLint *indic,
-                  GLint indicCount) {// Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+// Fill buffers for drawing polygons with vertex and indic values
+void bindPolygons(GLfloat *verts, GLint vertCount, GLint *indic, GLint indicCount) {
     glBindVertexArray(uiVertexArray);
 
     glBindBuffer(GL_ARRAY_BUFFER, uiVertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertCount * 3, verts, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertCount * VERTEX_COORDINATE_COUNT, verts, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, uiElementArray);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLint) * indicCount * 3, indic, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid *) 0);
+    glVertexAttribPointer(0, VERTEX_COORDINATE_COUNT, GL_FLOAT, GL_FALSE, VERTEX_COORDINATE_COUNT * sizeof(GLfloat),
+                          (GLvoid *) 0);
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0); // Unbind VAO
 }
 
+/*
+ * Various generation, binding, etc for Opengl
+ */
 void ui_init() {
+    uiShaderProgram = glCreateProgram();
     glGenVertexArrays(1, &uiVertexArray);
     glGenBuffers(1, &uiVertexBuffer);
     glGenBuffers(1, &uiElementArray);
-    uiShaderProgram = glCreateProgram();
 }
 
 void ui_draw() {
     glUseProgram(uiShaderProgram);
     glBindVertexArray(uiVertexArray);
-    glDrawElements(GL_TRIANGLES, uiTrianglesCount * 3, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, uiTrianglesCount * VERTEX_COORDINATE_COUNT, GL_UNSIGNED_INT, 0);
 }
 
+/*
+ * Reload graph shader program, vertices and indices from files
+ */
 void ui_reload() {
     GLint vertCount = getVectorCountFromFile("ui/vertices.txt");
     GLint indicCount = getVectorCountFromFile("ui/indices.txt");
-    GLfloat verts[vertCount * 3];
-    GLint indic[indicCount * 3];
+    GLfloat verts[vertCount * VERTEX_COORDINATE_COUNT];
+    GLint indic[indicCount * 3]; // 3 vertices per triangle
     getFloatVectorFromFile("vertices.txt", vertCount, verts);
     getIntVectorFromFile("indices.txt", indicCount, indic);
     bindPolygons(verts, vertCount, indic, indicCount);
