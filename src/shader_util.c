3700a4e37ac29f0d98edb117ef966c24ecf9de1d
Simen <Simennj@outlook.com>
2018-05-10 23:27:42 +0200

    add comments to shader utils source

--- src/shader_util.c
+++ src/shader_util.c
@@ -5,10 +5,14 @@
 const GLchar *vertexShaderFile = "vertexShader.glsl";
 const GLchar *fragmentShaderFile = "fragmentShader.glsl";
 
+/*
+ * Load shader source from file into shader
+ */
 void loadFromFile(GLuint shader, const GLchar *shaderFile) {
     const GLchar *shaderSource = getShaderSource(shaderFile);
     glShaderSource(shader, 1, &shaderSource, NULL);
     glCompileShader(shader);
+    // Free memory used for shader source after use
     free((void *) shaderSource);
     // Check for compile time errors
     GLint success;
@@ -20,17 +24,17 @@ void loadFromFile(GLuint shader, const GLchar *shaderFile) {
     }
 }
 
+/*
+ * Initializes the given shader program with shaders from the given folder
+ */
 void shader_programInit(GLuint shaderProgram, const GLchar *folder) {
     GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
     GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 
-
     int pathBufferSize = 50;
     char pathBuffer[pathBufferSize];
-
     // Load vertex shader
     loadFromFile(vertexShader, pathAppend(pathBuffer, pathBufferSize, folder, vertexShaderFile));
-
     // Load fragment shader
     loadFromFile(fragmentShader, pathAppend(pathBuffer, pathBufferSize, folder, fragmentShaderFile));
 
@@ -48,6 +52,7 @@ void shader_programInit(GLuint shaderProgram, const GLchar *folder) {
         printf("Shader linking error:%s\n", infoLog);
     }
 
+    // Clean up afterwards
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     glDetachShader(shaderProgram, vertexShader);
