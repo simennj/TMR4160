fad42ecf5319e971f08e65c857156023f0ddd5ad
Simen <simennj@outlook.com>
2018-05-10 20:08:05 +0200

    add comments to fake_boat source

--- src/fake_boat.c
+++ src/fake_boat.c
@@ -11,12 +11,18 @@ double fakeBoat_getPosition() {
     return position;
 }
 
+/*
+ * Calculates position and velocity change based on acceleration and velocity from last call and time elapsed since.
+ * Then updates the acceleration based on the input.
+ * All values are clamped to match the control of the real boat better
+ */
 void fakeBoat_update(double dt, double motor_force) {
     position = clamp(position + velocity * dt, -1, 1);
     velocity = clamp(velocity + acceleration * dt, -1, 1);
     acceleration = clamp(motor_force, -1, 1);
 }
 
+// Helper function used when debugging PID controller
 void fakeBoat_printState() {
     printf("Position: %f; Velocity: %f; Acceleration: %f\n", position, velocity, acceleration);
 }
